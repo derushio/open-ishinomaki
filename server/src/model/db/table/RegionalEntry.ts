85a4e0ba87759341d4993cb13704fefa497e7b2a
derushio <narumi18wa@gmail.com>
2017-07-28 18:13:36 +0900

    add RegionalEntryTable

---
+++ server/src/model/db/table/RegionalEntry.ts
@@ -0,0 +1,19 @@
+import Table, {Record} from "./Table"
+import Column from "../column/Column"
+import ColumnTypes from "../../../define/db/ColumnTypes"
+
+export default class RegionalEntryTable extends Table {
+    public static tableName: string = "regional_entry"
+    public static columns: Column[] = [
+        new Column("id", ColumnTypes.serial, false, null, true),
+        new Column("name", ColumnTypes.text, true),
+        new Column("category_ids", ColumnTypes.intArray, false),
+        new Column("imageUrls", ColumnTypes.textArray, false),
+        new Column("text", ColumnTypes.text, true)
+    ]
+}
+
+export class RegionalEntryRecord extends Record {}
+
+RegionalEntryTable.RecordClass = RegionalEntryRecord
+RegionalEntryRecord.TableClass = RegionalEntryTable
