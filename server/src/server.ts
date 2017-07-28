3d19426f748e096fa4ff0793d4ac178cf7a70abc
derushio <narumi18wa@gmail.com>
2017-07-28 22:28:29 +0900

    fix server location

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,10 +14,7 @@ createTables().then(() => {
 
     server.use(bodyParser.json({extended: true}))
     server.use(express.static("../web/"))
-    // このディレクトリ以下を公開するサーバーを起動
-    server.use(express.static("./"))
     server.listen(port, null)
-
 
     registRestApis(server)
 })
