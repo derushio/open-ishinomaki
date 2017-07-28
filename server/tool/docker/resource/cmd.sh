336e9fe0665677076d0e8f3ca6d993660c53f9ce
derushio <narumi18wa@gmail.com>
2017-07-28 22:28:20 +0900

    docker更新

--- server/tool/docker/resource/cmd.sh
+++ server/tool/docker/resource/cmd.sh
@@ -1,6 +1,7 @@
 service postgresql start
 
-# TODO ufw
+ufw allow 8000
+ufw enable
 
 echo "root:KsJaA4uQ" | chpasswd
 su - postgres -c "psql -U postgres -c \"CREATE ROLE root WITH LOGIN CREATEDB PASSWORD 'KsJaA4uQ';\""
--- server/tool/docker/run.sh
+++ server/tool/docker/run.sh
@@ -1,4 +1,7 @@
 docker build -t 'open-ishinomaki' ../../
 docker stop 'open-ishinomaki'
 docker rm 'open-ishinomaki'
-docker run -d --name 'open-ishinomaki' -v `cd ../../dist; pwd`:/etc/product/open-ishinomaki -p 8000:8000 -it 'open-ishinomaki'
+docker run --privileged -d --name 'open-ishinomaki' \
+    -v `cd ../../dist; pwd`:/etc/product/open-ishinomaki/server \
+    -v `cd ../../../web/dist; pwd`:/etc/product/open-ishinomaki/web \
+    -p 8000:8000 -it 'open-ishinomaki'
