06d2d123e7fa8a5b54f718b06b9d868001147590
derushio <narumi18wa@gmail.com>
2017-07-28 22:16:52 +0900

    change port

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,23 @@
 export declare function require(name: string): any
+const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 
 import createTables from "./logic/initData/createTables"
 import createInitData from "./logic/initData/createInitData"
+import registRestApis from "./logic/restApi/registRestApis"
 
 createTables().then(() => {
     return createInitData()
 }).then(() => {
-    const port = 3000
+    const port = 8000
 
+    server.use(bodyParser.json({extended: true}))
+    server.use(express.static("../web/"))
     // このディレクトリ以下を公開するサーバーを起動
     server.use(express.static("./"))
     server.listen(port, null)
+
+
+    registRestApis(server)
 })
--- server/tool/docker/run.sh
+++ server/tool/docker/run.sh
@@ -1,4 +1,4 @@
 docker build -t 'open-ishinomaki' ../../
 docker stop 'open-ishinomaki'
 docker rm 'open-ishinomaki'
-docker run -d --name 'open-ishinomaki' -v `cd ../../dist; pwd`:/etc/product/open-ishinomaki -p 8000:80 -it 'open-ishinomaki'
+docker run -d --name 'open-ishinomaki' -v `cd ../../dist; pwd`:/etc/product/open-ishinomaki -p 8000:8000 -it 'open-ishinomaki'
