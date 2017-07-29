065fc1c181a9d6e59331cadf8fbe23f35be2400a
Ryutaro Kimura <ryu31997@gmail.com>
2017-07-29 21:45:42 +0900

    アバウトです。

--- web/src/script/entry/page/about.ts
+++ web/src/script/entry/page/about.ts
@@ -1,5 +1,7 @@
 export declare function require(name: string): any
+
 const riot = require("riot")
 const test = require("../../tag/test.tag")
+const about2 = require("../../tag/about-img-2.tag")
 
 riot.mount("*")
