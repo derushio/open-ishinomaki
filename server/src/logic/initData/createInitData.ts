e626429402f387c001d9a91b9b913d3ee3c37a2f
derushio <narumi18wa@gmail.com>
2017-07-29 10:46:25 +0900

    regionalデータ登録

--- server/src/logic/initData/createInitData.ts
+++ server/src/logic/initData/createInitData.ts
@@ -1,8 +1,11 @@
 import DBPoolManager from "../../manager/DBPoolManager"
+import createInitRegional from "./createInitRegional"
 import createInitTags from "./createInitTags"
 
 export default function createInitData() {
     return DBPoolManager.getInstance().then((dbpm: DBPoolManager) => {
-        return createInitTags(dbpm)
+        return createInitRegional(dbpm).then(() => {
+            return createInitTags(dbpm)
+        })
     })
 }
