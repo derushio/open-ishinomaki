46467b39b23a5f6e8c2d048f347cf9d8030f1751
derushio <narumi18wa@gmail.com>
2017-07-29 10:34:53 +0900

    add comment

--- server/src/model/db/table/RegionalTable.ts
+++ server/src/model/db/table/RegionalTable.ts
@@ -2,6 +2,15 @@ import Table, {Record} from "./Table"
 import Column from "../column/Column"
 import ColumnTypes from "../../../define/db/ColumnTypes"
 
+/**
+ * 地域テーブル
+ * @param  {[type]} "id"               [description]
+ * @param  {[type]} ColumnTypes.serial [description]
+ * @param  {[type]} false              [description]
+ * @param  {[type]} null               [description]
+ * @param  {[type]} true               [description]
+ * @return {[type]}                    [description]
+ */
 export default class RegionalTable extends Table {
     public static tableName: string = "regional_entry"
     public static columns: Column[] = [
