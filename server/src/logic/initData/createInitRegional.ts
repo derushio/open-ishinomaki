e9f8bf4334468bdd7aaa58e6d0ad73a7731425c4
derushio <narumi18wa@gmail.com>
2017-07-29 10:45:21 +0900

    初期地域データを登録

---
+++ server/src/logic/initData/createInitRegional.ts
@@ -0,0 +1,24 @@
+import DBPoolManager from "../../manager/DBPoolManager"
+import RegionalTable, {RegionalRecord, createRegional} from "../../model/db/table/RegionalTable"
+
+/**
+ * 初期タグを登録
+ * @param  {DBPoolManager} dbpm [description]
+ * @return {Promise<any>}       [description]
+ */
+export default function createInitTags(dbpm: DBPoolManager): Promise<any> {
+    return Promise.all([
+        createRegional(dbpm, "山下", [
+            "1丁目", "2丁目"
+        ]),
+        createRegional(dbpm, "西山", [
+            "1丁目", "2丁目"
+        ]),
+        createRegional(dbpm, "田道町", [
+            "1丁目", "2丁目"
+        ]),
+        createRegional(dbpm, "中央", [
+            "1丁目", "2丁目"
+        ])
+    ])
+}
