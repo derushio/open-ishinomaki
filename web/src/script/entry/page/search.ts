77e1ee11e260d3768617f046d0ee5bbb9f6c0a1e
derushio <narumi18wa@gmail.com>
2017-07-29 15:15:56 +0900

    import

--- web/src/script/entry/page/search.ts
+++ web/src/script/entry/page/search.ts
@@ -1,6 +1,9 @@
 export declare function require(name: string): any
 const riot = require("riot")
 
+const regionalSearch = require("../../tag/part/regional-search.tag")
+const tagSearch = require("../../tag/part/tag-search.tag")
+
 const search = require("../../tag/part/search.tag")
 
 riot.mount("*")
