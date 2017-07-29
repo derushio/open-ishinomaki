26ff45fe79e35386c8be601264f53cf84985f44a
derushio <narumi18wa@gmail.com>
2017-07-29 15:10:29 +0900

    fix indent

--- web/src/script/entry/page/search.ts
+++ web/src/script/entry/page/search.ts
@@ -1,6 +1,6 @@
 export declare function require(name: string): any
 const riot = require("riot")
-const test = require("../../tag/test.tag")
+
 const search = require("../../tag/part/search.tag")
 
 riot.mount("*")
