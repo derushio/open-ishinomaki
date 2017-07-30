fbacfbf51e7944c744008b17abf9ec2606ad0867
derushio <narumi18wa@gmail.com>
2017-07-30 10:55:58 +0900

    write basecontent

---
+++ web/src/script/entry/page/api.ts
@@ -0,0 +1,8 @@
+export declare function require(name: string): any
+const riot = require("riot")
+
+const commonHeader = require("../../tag/section/common-header.tag")
+
+const test = require("../../tag/test.tag")
+
+riot.mount("*")
